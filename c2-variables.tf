ede57b6214194e51b465d5cb5654b18a77083b96
Katarzyna Krolikowska <cathy1721@gmail.com>
2023-01-28 13:24:40 +0100

    formatting

--- c2-variables.tf
+++ c2-variables.tf
@@ -23,17 +23,17 @@ variable "instance_keypair" {
 # AWS EC2 Instance Type - List
 variable "instance_type_list" {
   description = "EC2 Instance Type"
-  type = list(string)
-  default = [ "t3.micro", "t3.small", "t3.large" ]
+  type        = list(string)
+  default     = ["t3.micro", "t3.small", "t3.large"]
 }
 
 # AWS EC2 Instance Type - Map
 variable "instance_type_map" {
   description = "EC2 Instance Type"
-  type = map(string)
+  type        = map(string)
   default = {
-    "dev" = "t3.micro"
-    "qa" = "t3.small"
+    "dev"  = "t3.micro"
+    "qa"   = "t3.small"
     "prod" = "t3.large"
   }
 }
